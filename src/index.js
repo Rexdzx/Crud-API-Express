68780419fc3be8d4c1908d9e524aad23d392cafc
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-12-13 19:42:33 +0700

    set CORS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
-const app = express();//inisialisasi express
-app.use(express.json());//untuk parsing data
+const app = express(); //inisialisasi express
+app.use(express.json()); //untuk parsing data
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+app.use(cors());
 
 app.get("/api", (req, res) => {
-    res.send("Hello");
+  res.send("Hello");
 });
 
 const BookController = require("./books/book.controller");
@@ -16,5 +19,5 @@ const BookController = require("./books/book.controller");
 app.use("/books", BookController); //'books', sudah menjadi prefix, jadi books yang ada di controller dihapus
 
 app.listen(PORT, () => {
-    console.log(`Server is Running in Port: ${PORT}`);
+  console.log(`Server is Running in Port: ${PORT}`);
 });
